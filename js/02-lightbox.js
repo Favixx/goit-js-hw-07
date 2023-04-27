d5bfed4707952e42189f5af6679bebbf911a93be
Favixx <bogdandom52@gmail.com>
2023-04-27 16:33:15 +0300

    homework finish

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+const galleryItemsMarkup = galleryItems.map(el => 
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${el.original}">
+        <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+    </a>
+    </li>`
+).join('');
+galleryList.innerHTML = galleryItemsMarkup;
+
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionsPosition: 'bottom'
+});
